e010bc9e6969c5394b1c202bc9d71f95f7297d57
Kili S <79143155+klinshy@users.noreply.github.com>
2024-09-23 13:24:46 +0200

    Dynamic Building Title

--- src/smallOffice-main.ts
+++ src/smallOffice-main.ts
@@ -33,4 +33,32 @@ function closePopup(){
     }
 }
 
+// Get an existing website object where 'my_website' is the name of the object (on any layer object of the map)
+async function updateTitle() {
+    var text: string = WA.state.textVariable as string;
+    console.log('Text is configured as ' + text);
+    var color: string = WA.state.colorVariable as string;
+    console.log('Color is configured as ' + color);
+    var newTitle = `https://iw6tkif7th7yp5ax2ufzkl3kce0bcuys.lambda-url.us-east-1.on.aws/?text=${encodeURIComponent(text)}&imageType=caption&width=88&height=24&color=${encodeURIComponent(color)}`;
+    console.log('New img-url of title is ' + newTitle);
+    const website = await WA.room.website.get("titleScreen");
+    website.url = newTitle;
+    website.visible = true;
+    console.log(`Title has been changed to ${website.url}`);
+};
+WA.onInit().then(() => { updateTitle(); });
+
+
+// Listen for changes to the textVariable
+WA.state.onVariableChange('textVariable').subscribe(() => {
+    console.log(`Text variable changed`);
+    updateTitle();
+});
+
+// Listen for changes to the colorVariable
+WA.state.onVariableChange('colorVariable').subscribe(() => {
+    console.log(`Color variable changed`);
+    updateTitle();
+});
+
 export {};
