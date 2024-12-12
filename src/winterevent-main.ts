a205866f01e7d71ffa559a9a70764c50dcd023b8
Kili S <79143155+klinshy@users.noreply.github.com>
2024-12-12 14:45:40 +0100

    xmas map stuff

--- src/winterevent-main.ts
+++ src/winterevent-main.ts
@@ -3,23 +3,19 @@
 import { bootstrapExtra,Properties } from "@workadventure/scripting-api-extra";
 import { Subscription } from "rxjs";
 import { checkPlayerMaterial, mySound, playRandomSound } from "./footstep";
+import { santaMessages } from "./santa";
+import { levelUp, getLeaderboard, getLeaderboardURL } from "@workadventure/quests";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags);
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    });
-
-    WA.room.area.onLeave('clock').subscribe(closePopup);
+console.log("Properties loaded: ", Properties);
+console.log ("Quests loaded: ", levelUp);
+console.log ("Leaderboard loaded: ", getLeaderboard);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -38,12 +34,6 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 }).catch(e => console.error(e));
 
-function closePopup() {
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 WA.onInit()
   .then(() => {
     const startIceSkating = () => {
@@ -72,129 +62,175 @@ WA.onInit()
       });
     };
 
+    WA.onInit().then(() => {
+      let skatingSub: Subscription;
 
-    let skatingSub: Subscription;
+      WA.room.area.onEnter("ice").subscribe(() => {
+        console.log("enter");
+        skatingSub = startIceSkating();
+      });
 
-    WA.room.area.onEnter("ice").subscribe(() => {
-      console.log("enter");
-      skatingSub = startIceSkating();
+      WA.room.area.onLeave("ice").subscribe(() => {
+        console.log("leave");
+        skatingSub.unsubscribe();
+      });
+      })
+      .catch((e) => console.error(e));
     });
 
-    WA.room.area.onLeave("ice").subscribe(() => {
-      console.log("leave");
-      skatingSub.unsubscribe();
-    });
-  })
-  .catch((e) => console.error(e));
+      WA.event.on('pong').subscribe((value: any) => {
+      const placedGifts = value.data;
+      console.log('Received placed gifts:', placedGifts);
+      WA.room.setTiles(placedGifts);
+      WA.state.saveVariable('placedGifts', placedGifts);
+      });
 
-  WA.onInit().then(() => {
-    console.log('WA.onInit called');
-    WA.room.getTiledMap().then(map => {
-        console.log('Tiled map retrieved');
-        const mapProperties = new Properties(map.properties);
+      WA.event.on('foundGift').subscribe(async (value: any) => {
+      const { x, y } = value.data;
+      console.log(`Gift found at (${x}, ${y})`);
+      WA.room.setTiles([{ x, y, tile: null, layer: "gifts" }]);
+
+      const placedGifts = (await WA.state.loadVariable('placedGifts')) as any[];
+      const updatedGifts = placedGifts.filter(gift => gift.x !== x || gift.y !== y);
+      WA.state.saveVariable('placedGifts', updatedGifts);
+      });
+
+      WA.room.onEnterLayer("gifts").subscribe(async () => {
+      console.log('Player entered gifts layer');
+      const { x, y } = await WA.player.getPosition();
+      console.log('Player position in pixels:', { x, y });
+      const tileX = Math.floor(x / 32);
+      const tileY = Math.floor(y / 32);
+
+      const randomMessageIndex = Math.floor(Math.random() * santaMessages.length);
+      const randomMessage = santaMessages.splice(randomMessageIndex, 1)[0];
+      const mySound = WA.sound.loadSound("./pickup3.wav");
+      const config = {
+        volume: 0.5,
+        loop: false,
+        rate: 1,
+        detune: 1,
+        delay: 0,
+        seek: 0,
+        mute: false
+      };
+      mySound.play(config);
+      WA.event.broadcast('foundGift', { x: tileX, y: tileY });
+      WA.chat.sendChatMessage(randomMessage, "Santa");
+      await levelUp("GIFTS", 1)
+      });
+
+      WA.onInit().then(async () => {
+      const placedGifts = (await WA.state.loadVariable('placedGifts')) as any[];
+      if (placedGifts && placedGifts.length > 0) {
+        WA.room.setTiles(placedGifts);
+      }
+      });
+
+      WA.onInit().then(async () => {
+      WA.player.onPlayerMove(async ({ x, y, moving }) => {
+        const material = await checkPlayerMaterial({ x, y });
+        console.log(material);
+
+        if (!material) {
+        return mySound?.stop();
+        }
+
+        if (!moving && !material) {
+        return mySound?.stop();
+        } else {
+        mySound?.stop();
+        return playRandomSound(material);
+        }
+      });
+      });
+
+      WA.onInit().then(() => {
+      if (WA.player.name === 'Santa') {
+        WA.event.on('ping').subscribe(async (value: any) => {
+        if (value.data === 'start') {
+          console.log('Starting gift population...');
+          const placedGifts = await populateGifts();
+          WA.event.broadcast('pong', placedGifts);
+        }
+        });
+
+        async function populateGifts() {
+        const map = await WA.room.getTiledMap();
+        console.log("Room Height: ", map.height);
+        console.log("Room Width: ", map.width);
+
+        const mapProperties = new Properties(map.properties); 
         const collisionLayerName = mapProperties.getString('collisionLayerName') || 'collisions';
-
         const collisionLayer = map.layers.find(layer => layer.name === collisionLayerName);
-        if (collisionLayer && collisionLayer.type === 'tilelayer') {
-            const tiles = collisionLayer.data;
-            const width = collisionLayer.width;
-            const height = collisionLayer.height;
 
-            for (let y = 0; y < height; y++) {
-                for (let x = 0; x < width; x++) {
-                    const tileIndex = y * width + x;
-                    if (tiles[tileIndex] !== 0) { // Assuming 0 means no tile
-                        console.log(`Tile at (${x}, ${y})`);
-                    }
-                }
-            }
-        } else {
-            console.error('Collision layer not found or is not a tile layer');
+        if (!collisionLayer || collisionLayer.type !== 'tilelayer') {
+          console.error('Collision layer not found or is not a tile layer');
+          return [];
         }
-    }).catch(e => console.error('Error retrieving tiled map:', e));
-}).catch(e => console.error('Error during WA.onInit:', e));
 
-async function populateGifts() {
-    const map = await WA.room.getTiledMap();
-    console.log("Room Height: ", map.height);
-    console.log("Room Width: ", map.width);
+        const tiles = collisionLayer.data;
+        const width = collisionLayer.width;
+        const height = collisionLayer.height;
 
-    const mapProperties = new Properties(map.properties);
-    const collisionLayerName = mapProperties.getString('collisionLayerName') || 'collisions';
-    const collisionLayer = map.layers.find(layer => layer.name === collisionLayerName);
-
-    if (!collisionLayer || collisionLayer.type !== 'tilelayer') {
-        console.error('Collision layer not found or is not a tile layer');
-        return;
-    }
-
-    const tiles = collisionLayer.data;
-    const width = collisionLayer.width;
-    const height = collisionLayer.height;
-
-    const potentialGiftTiles = [];
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            const tileIndex = y * width + x;
-            if (tiles[tileIndex] === 0) { // Assuming 0 means no collision
-                potentialGiftTiles.push({ x, y });
-            }
+        const potentialGiftTiles = [];
+        for (let y = 0; y < height; y++) {
+          for (let x = 0; x < width; x++) {
+          const tileIndex = y * width + x;
+          if (tiles[tileIndex] === 0) { // Assuming 0 means no collision
+            potentialGiftTiles.push({ x, y });
+          }
+          }
         }
-    }
 
-    const giftOptions = 8;
-    const giftsToPlace = 10; // Number of gifts to place
-    const placedGifts: { x: number, y: number, tile: string, layer: string }[] = [];
+        const giftOptions = 8;
+        const giftsToPlace = 30; // Number of gifts to place
+        const placedGifts: { x: number, y: number, tile: string, layer: string }[] = [];
 
-    for (let i = 0; i < giftsToPlace; i++) {
-        const randomIndex = Math.floor(Math.random() * potentialGiftTiles.length);
-        const { x, y } = potentialGiftTiles.splice(randomIndex, 1)[0];
-        const giftTile = `gift_${Math.floor(Math.random() * giftOptions) + 1}`;
-        placedGifts.push({ x, y, tile: giftTile, layer: "gifts" });
-    }
-
-    WA.room.setTiles(placedGifts);
-    console.log('Gifts placed:', placedGifts);
-
-    WA.room.onEnterLayer("gifts").subscribe(async () => {
-        console.log('Player entered gifts layer');
-        const { x, y } = await WA.player.getPosition();
-        console.log('Player position in pixels:', { x, y });
-        const tileX = Math.floor(x / 32);
-        const tileY = Math.floor(y / 32);
-        console.log('Player position in tiles:', { tileX, tileY });
-
-        const giftIndex = placedGifts.findIndex(gift => gift.x === tileX && gift.y === tileY);
-        if (giftIndex !== -1) {
-            console.log('Gift found at player position');
-            WA.chat.sendChatMessage("You found a gift!", "Game");
-            WA.room.setTiles([{ x: tileX, y: tileY, tile: null, layer: "gifts" }]);
-            placedGifts.splice(giftIndex, 1);
-        } else {
-            console.log('No gift at player position');
+        for (let i = 0; i < giftsToPlace; i++) {
+          const randomIndex = Math.floor(Math.random() * potentialGiftTiles.length);
+          const { x, y } = potentialGiftTiles.splice(randomIndex, 1)[0];
+          const giftTile = `gift_${Math.floor(Math.random() * giftOptions) + 1}`;
+          placedGifts.push({ x, y, tile: giftTile, layer: "gifts" });
         }
-    });
-}
 
-populateGifts().catch(e => console.error('Error populating gifts:', e));
+        console.log('Gifts placed:', placedGifts);
+        return placedGifts;
+        }
+      }
+      });
 
+      WA.onInit().then(() => {
+        WA.room.area.onEnter('santasCrashSite').subscribe(() => {const leaderboardURL = getLeaderboardURL("QUEST_KEY");
+
+          // Open a modal with the leaderboard
+          WA.ui.modal.openModal({
+              title: "GiftsQuest Leaderboard",
+              src: leaderboardURL.toString(),
+              position: "left",
+              allowApi: false,
+              allow: "",
+          }, () => {});})});
 
 WA.onInit().then(async () => {
-  WA.player.onPlayerMove(async ({ x, y, moving }) => {
-    const material = await checkPlayerMaterial({ x, y });
-    console.log(material);
+  if (WA.player.name === 'Santa') {
+    const initialPosition = await WA.player.getPosition();
+    const { x: startX, y: startY } = initialPosition;
 
-    if (!material) {
-      return mySound?.stop();
-    }
+    const moveSantaRandomly = () => {
+      const minX = Math.max(startX - 32, 0);
+      const maxX = startX + 32;
+      const minY = Math.max(startY - 32, 0);
+      const maxY = startY + 32;
 
-    if (!moving && !material) {
-      return mySound?.stop();
-    } else {
-      mySound?.stop();
-      return playRandomSound(material);
-    }
-  });
+      const randomX = Math.random() * (maxX - minX) + minX;
+      const randomY = Math.random() * (maxY - minY) + minY;
+
+      console.log(`Santa moving to random position: x=${randomX}, y=${randomY}`);
+      WA.player.moveTo(randomX, randomY, 1);
+    };
+
+    setInterval(moveSantaRandomly, Math.random() * 5000 + 2000); // Random interval between 2 to 7 seconds
+  }
 });
-
 export {};
