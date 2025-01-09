b3c16ae285176a593c90ab302df7c397567cee45
Kili S <79143155+klinshy@users.noreply.github.com>
2025-01-09 12:30:42 +0100

    updated scripting api extra version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-console.log('Script started successfully');
+
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
 
 type FloorName = 'top' | 'middle' | 'bottom';
 const LAYERS = {
